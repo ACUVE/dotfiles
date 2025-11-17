e10f3884f373895e34397abe444f18f29750b65f
ACUVE <sub_chon@yahoo.co.jp>
2025-11-17 13:57:52 +0900

    define .sh_profile

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -eux
+set -euxo pipefail
 
 git submodule update --init --recursive || true
 
 # Settings #####
-DOT_FILES=(.zsh .zshrc .zshenv .vimrc .vim .easystroke .tmux.conf .shenv .pep8 .pyenv .digrc .bin .bashrc)
+DOT_FILES=(.zsh .zshrc .zshenv .vimrc .vim .easystroke .tmux.conf .shenv .pep8 .pyenv .digrc .bin .bashrc .sh_profile .bash_profile)
 DOT_DIRECTORY_EACH_FILE=(.ssh .aws .awsume)
 CONFIG_FILES=(nvim git mise)
 OLD_DOT_FILES=(.env)  # 過去管理していたが、現在は管理していないファイル群
