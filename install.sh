086299aaeb382a16c48fe80ca7fb33ca71b0dc4f
ACUVE <sub_chon@yahoo.co.jp>
2025-11-20 22:14:10 +0900

    gitignore を削除（Gemini3 はアホ）

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 git submodule update --init --recursive || true
 
 # Settings #####
-DOT_FILES=(.zsh .zshrc .zshenv .vimrc .vim .easystroke .tmux.conf .shenv .pep8 .pyenv .digrc .bin .bashrc .sh_profile .bash_profile .gitignore)
+DOT_FILES=(.zsh .zshrc .zshenv .vimrc .vim .easystroke .tmux.conf .shenv .pep8 .pyenv .digrc .bin .bashrc .sh_profile .bash_profile)
 DOT_DIRECTORY_EACH_FILE=(.ssh .aws .awsume)
 CONFIG_FILES=(nvim git mise pnpm)
-OLD_DOT_FILES=(.env)  # 過去管理していたが、現在は管理していないファイル群
+OLD_DOT_FILES=(.env .gitignore)  # 過去管理していたが、現在は管理していないファイル群
 OLD_CONFIG_FILES=(claude)  # 過去管理していたが、現在は管理していないファイル群
 ################
 
