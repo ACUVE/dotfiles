e5e077df03884850cf6ceddbf73537ec55444d59
ACUVE <601805+ACUVE@users.noreply.github.com>
2025-12-11 21:36:03 +0900

    pragma: no cover on the except line in src/cli/__init__.py

--- packages/python/cli/src/cli/__init__.py
+++ packages/python/cli/src/cli/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version(__name__)
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0"
 
 __all__ = ["main", "__version__"]
